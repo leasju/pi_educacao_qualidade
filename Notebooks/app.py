5fa1d3e38560b7a576d35c79d4ba8984c4ed6ab7
Julia Leandro <jusouzaleandro@gmail.com>
2026-05-01 18:54:11 -0300

    fix:  ajusta import do notebook no app.py para evitar erro de tipagem ao importar PI_ANÁLISE

--- Notebooks/app.py
+++ Notebooks/app.py
@@ -3,8 +3,11 @@ import streamlit as st
 
 # Importar o notebook como módulo
 with importnb.Notebook():
-    import PI_ANÁLISE as pi
+    import PI_ANÁLISE as pi # type: ignore
 
 # Dashboard interativo
 for itens in pi.arquivos.values():
     pi.visualizar(itens)
+
+
+    
