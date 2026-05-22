35763c611010f806c62916ca80498bec5fa0612e
Julia Leandro <jusouzaleandro@gmail.com>
2026-05-22 20:54:22 -0300

    Refactor index.html and app.py for improved structure and functionality; add style.css for styling

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
+import plotly.express as px
 from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="Template")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    data_canada = px.data.gapminder().query("country == 'Canada'")
+
+    fig = px.bar(
+        data_canada,
+        x='year',
+        y='pop',
+        title='População do Canadá'
+    )
+
+    graph_html = fig.to_html(full_html=False)
+
+    return render_template(
+        "index.html", graph_html=graph_html)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
+
+
