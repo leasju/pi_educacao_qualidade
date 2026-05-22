86fa2f8ce7adf1b37f37072bfcc6be498fefdf81
Julia Leandro <jusouzaleandro@gmail.com>
2026-05-22 10:22:37 -0300

    Edição do caminho do arquivo

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+caminho_index = os.path.join(BASE_DIR, "Template", "index.html")
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(caminho_index)
